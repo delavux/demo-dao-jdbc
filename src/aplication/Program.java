22bf513a4d3161a03bd14c340c32adc8d946941f
Lucas Samuel <lucas.a.ucb.br@gmail.com>
2025-09-10 19:11:35 -0300

    reutilizando Instanciação

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -12,6 +12,8 @@ public class Program {
         Department obj = new Department(1, "books");
         System.out.println(obj);
 
+
+        System.out.println("=== TEST 1: seller findByid ===");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
 
